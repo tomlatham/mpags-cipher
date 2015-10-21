7586574785019290b967165ad737a964a05c2ffd
Thomas Latham <T.Latham@warwick.ac.uk>
2015-10-21 13:31:07 +0100

    Remove main program temporarily

--- mpags-cipher.cpp
+++
@@ -1,8 +0,0 @@
-//! Main implementation of the mpags-cipher program
-
-// This is one of the two valid signatures for main()
-int main(int /*argc*/, char* /*argv*/[]) {
-  // No requirement to return from main, but we do so for clarity and
-  // consistency with other functions.
-  return 0;
-}
